07498ea5ed4b424380987c7e4ac55e3acb129a86
Frank Park <jehyn923@gmail.com>
2021-11-25 04:39:26 +0900

    fix:enquiry

--- src/main/java/com/ssafy/happyhouse/enquiry/repository/EnquiryRepository.java
+++ src/main/java/com/ssafy/happyhouse/enquiry/repository/EnquiryRepository.java
@@ -1,7 +1,9 @@
 package com.ssafy.happyhouse.enquiry.repository;
 
 import com.ssafy.happyhouse.enquiry.entity.Enquiry;
+import com.ssafy.happyhouse.realty.entity.Option;
 import com.ssafy.happyhouse.realty.entity.Realty;
+import com.ssafy.happyhouse.user.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
